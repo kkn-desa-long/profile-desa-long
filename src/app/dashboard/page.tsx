23e2f9116686ef5aeb13a93aba8ea607503c701d
Farhan <anak.upload90@gmail.com>
2025-07-15 01:24:44 +0800

    feat: update dashboard page to display welcome message

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <section className="">
+      <h1 className="text-3xl font-bold">Selamat Datang di Dashboard Website Profil Desa Long</h1>
+    </section>
+  );
 }
